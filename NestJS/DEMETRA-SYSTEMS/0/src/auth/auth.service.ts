fe395c161e591f6b53eafcd16a998199919ee1eb
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-23 23:33:42 +0600

    Adding SIGNUP && SIGNIN

--- NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.service.ts
+++ NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {  // SAVE
+      const user = await this.prisma.user.create({
+        data: {
+          name: dto.name,
+          email: dto.email,
+          password: hash
+        }
+      });
+
+      delete user.password;
+      return user;
+    } catch (error) {  // ERROR
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new HttpException(
+            "ERR_USER_EMAIL_EXISTS",
+            HttpStatus.BAD_REQUEST
+          );
+        }
+      }
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    });
+    console.log(user);
+    if (!user) {
+      throw new HttpException(
+        "ERR_USER_EMAIL_EXISTS",
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    const passwordVerify = await argon.verify(
+      user.password,
+      dto.password
+    );
+    console.log(passwordVerify);
+    if (!passwordVerify) {
+      throw new HttpException(
+        "ERR_USER_PASSWORD_EXISTS",
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    delete user.password;
+    return user;
+  }
+}
