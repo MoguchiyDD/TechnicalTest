089bb16eb250450cf3bd3fe89749ca3a928d975c
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-23 23:33:42 +0600

    Adding SIGNUP && SIGNIN

--- NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.controller.ts
+++ NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post("signup")
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+
+  @Post("signin")
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
+}
