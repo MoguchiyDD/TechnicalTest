d04f5e017b4ea5749e4d83956eab8ea654a2fbad
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-23 19:32:00 +0600

    Adding The PRISMA Module

--- NestJS/DEMETRA-SYSTEMS/0/src/app.module.ts
+++ NestJS/DEMETRA-SYSTEMS/0/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule]
+  imports: [
+    PrismaModule,
+    AuthModule
+  ]
 })
 export class AppModule {}
