49b7b3a6ac45e9e3a36ce0624f7db93f5861537b
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-23 19:43:52 +0600

    Adding CONFIG

--- NestJS/DEMETRA-SYSTEMS/0/src/app.module.ts
+++ NestJS/DEMETRA-SYSTEMS/0/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     PrismaModule,
     AuthModule
   ]
