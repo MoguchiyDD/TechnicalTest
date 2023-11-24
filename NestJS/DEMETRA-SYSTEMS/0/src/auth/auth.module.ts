0a3a336f56f62e6f21170503d5d0b0b7f7a22f9c
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-25 01:47:02 +0600

    Adding The CachingModule

--- NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.module.ts
+++ NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BullModule } from '@nestjs/bull';
 import { BULL_NAME_REGISTER } from './consumer/auth.env';
 import { AuthQueue } from './consumer/auth.consumer';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
+import type { RedisClientOptions } from 'redis';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: BULL_NAME_REGISTER
+    }),
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get("REDIS_HOST"),
+        port: configService.get("REDIS_PORT"),
+        ttl: 1800  // seconds
+      }),
+      inject: [ConfigService],
     })
   ],
   providers: [AuthService, AuthQueue],
