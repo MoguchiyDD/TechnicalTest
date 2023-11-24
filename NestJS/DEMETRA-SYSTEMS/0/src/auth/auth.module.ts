923b7f99c6d1befa0a805b612cd8ba2717a00ec0
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-24 18:25:06 +0600

    Adding The BullModule : REGISTER

--- NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.module.ts
+++ NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+import { BULL_NAME_REGISTER } from './consumer/auth.env';
+import { AuthQueue } from './consumer/auth.consumer';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    BullModule.registerQueue({
+      name: BULL_NAME_REGISTER
+    })
+  ],
+  providers: [AuthService, AuthQueue],
   controllers: [AuthController]
 })
 export class AuthModule {}
