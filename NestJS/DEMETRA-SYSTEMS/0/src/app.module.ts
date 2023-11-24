c0c6cfaa8955564a258153416e92a7d06c19bdd3
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-24 18:24:13 +0600

    Adding The BullModule : ROOT

--- NestJS/DEMETRA-SYSTEMS/0/src/app.module.ts
+++ NestJS/DEMETRA-SYSTEMS/0/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -7,6 +8,16 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get("REDIS_HOST"),
+          port: configService.get("REDIS_PORT"),
+        },
+      }),
+      inject: [ConfigService],
     }),
     PrismaModule,
     AuthModule
