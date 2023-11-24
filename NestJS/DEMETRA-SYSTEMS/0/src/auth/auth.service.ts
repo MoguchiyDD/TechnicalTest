08a50c5fa9387c280e0b3a994e4dca351a2f534a
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-25 01:48:31 +0600

    Adding The CACHE

--- NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.service.ts
+++ NestJS/DEMETRA-SYSTEMS/0/src/auth/auth.service.ts
@@ -1,9 +1,12 @@
-import { HttpException, HttpStatus, Injectable, ParseIntPipe } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { isString } from 'class-validator';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
 import { BULL_NAME_PROCESS } from './consumer/auth.env';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 import { AuthUpDto, AuthInDto } from './dto';
 import * as argon from 'argon2';
 
@@ -11,8 +14,53 @@ import * as argon from 'argon2';
 export class AuthService {
   constructor(
     private prisma: PrismaService,
-    @InjectQueue("signs") private readonly signsQueue: Queue
+    @InjectQueue("signs") private readonly signsQueue: Queue,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
   ) {}
+  private readonly regexId = /^[0-9]+$/;
+
+  async userById(id: string) {
+    if (!isString(id)) {  // ERROR : Type Date
+      throw new HttpException(
+        "ERR_NO_ID_USER_EXISTS",
+        HttpStatus.NOT_FOUND
+      );
+    }
+
+    const throwUserNotFound = new HttpException(
+      "ERR_USER_NOT_FOUND",
+      HttpStatus.NOT_FOUND
+    );
+
+    if (this.regexId.test(id)) {
+      const idNum = parseInt(id);
+      const cacheUserData = await this.cacheManager.get("cache-user_" + id);
+      if ((cacheUserData) && (idNum === cacheUserData["id"])) {  // GET
+        const result = {
+          statusCode: 200,
+          message: "SUCCESS",
+          user: cacheUserData
+        };
+        return result;
+      }
+
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id: idNum
+        }
+      });
+      if (!user) { throw throwUserNotFound }
+
+      // SET
+      await this.cacheManager.set("cache-user_" + id, user)
+      const result = {
+        statusCode: 200,
+        message: "SUCCESS",
+        user: user
+      };
+      return result;
+    } else { throw throwUserNotFound };
+  }
 
   async signup(dto: AuthUpDto) {
     const hash = await argon.hash(dto.password);
@@ -81,12 +129,19 @@ export class AuthService {
 
   async signout(id: string) {
     let errorSignOutOn = 1
-
-    const regex = /^[0-9]+$/
-    if (regex.test(id)) {
+    if (this.regexId.test(id)) {
       --errorSignOutOn;
-
       const idNum = parseInt(id);
+      const cacheUserData = await this.cacheManager.get("cache-user_" + id);
+      if ((cacheUserData) && (idNum === cacheUserData["id"])) {
+        await this.signsQueue.add(  // STATUS
+          BULL_NAME_PROCESS,
+          { mode: "out", user: cacheUserData },
+          { delay: 10000 }
+        );
+        return;
+      }
+
       const user = await this.prisma.user.findUnique({
         where: {
           id: idNum
@@ -99,6 +154,7 @@ export class AuthService {
         { mode: "out", user: user },
         { delay: 10000 }
       );
+      return;
     }
 
     if (errorSignOutOn >= 1) {
