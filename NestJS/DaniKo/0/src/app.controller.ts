9fdbc928782d8daebb405576b0ca1114f9844d5c
MoguchiyDD <ket-ket-01@bk.ru>
2023-12-05 23:35:17 +0600

    Update '/'

--- NestJS/DaniKo/0/src/app.controller.ts
+++ NestJS/DaniKo/0/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async whastappWeb(): Promise<void> {
+    return this.appService.whastappWeb();
   }
 }
