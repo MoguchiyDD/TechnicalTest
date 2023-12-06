ad0023b29978d8a57094de10a633bc2a4fe09154
MoguchiyDD <ket-ket-01@bk.ru>
2023-12-06 21:40:34 +0600

    Update PORT

--- NestJS/DaniKo/0/src/main.ts
+++ NestJS/DaniKo/0/src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = 5000 || 3000;
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(port, () => console.log(`Server started on port ${port}`));
+  await app.listen(3000, () => console.log(`Server started on port ${port}`));
 }
 bootstrap();
