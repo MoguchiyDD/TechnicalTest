773fbcce0386f288a8037cae0407f2e4178b85fc
MoguchiyDD <ket-ket-01@bk.ru>
2023-12-07 02:56:27 +0600

    Adding CacheModule

--- NestJS/DaniKo/0/src/whatsapp/whatsapp.module.ts
+++ NestJS/DaniKo/0/src/whatsapp/whatsapp.module.ts
@@ -3,9 +3,24 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { WhatsAppSchema } from './whatsapp.model';
 import { WhatsAppService } from './whatsapp.service';
 import { WhatsAppController } from './whatsapp.controller';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
+import type { RedisClientOptions } from 'redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'WhatsApp', schema: WhatsAppSchema }])],
+  imports: [
+    CacheModule.registerAsync<RedisClientOptions>({
+      extraProviders: [],
+      useFactory: async (configService: ConfigService): Promise<any> => ({
+        store: redisStore,
+        host: configService.get("REDIS_HOST"),
+        port: configService.get("REDIS_PORT")
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([{ name: 'WhatsApp', schema: WhatsAppSchema }])
+  ],
   providers: [WhatsAppService],
   controllers: [WhatsAppController]
 })
