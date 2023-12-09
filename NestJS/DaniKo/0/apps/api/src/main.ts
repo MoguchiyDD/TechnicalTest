c4795ca819854ceb96f537e40f8d005311e84d26
MoguchiyDD <ket-ket-01@bk.ru>
2023-12-09 08:44:59 +0600

    Adding Global Prefix

--- NestJS/DaniKo/0/apps/api/src/main.ts
+++ NestJS/DaniKo/0/apps/api/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(3000, () => console.log(`Server started on port ${port}`));
 }
 bootstrap();
