81f069f8ce075b3ec846b8c6b0c15c1ecf4c0f1a
MoguchiyDD <ket-ket-01@bk.ru>
2023-12-09 08:46:07 +0600

    Adding 1st RUN

--- NestJS/DaniKo/0/apps/client/src/App.jsx
+++ NestJS/DaniKo/0/apps/client/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [whatsapp, setWhatsApp] = useState('')
+
+  useEffect(() => {
+    fetch('/api')
+    .then((res) => res.text())
+    .then(setWhatsApp);
+  }, []);
 
   return (
     <>
@@ -16,18 +22,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{whatsapp}</h1>
     </>
   )
 }
