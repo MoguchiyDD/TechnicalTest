837b60d1daf378d73e5e41fc878506e9da833696
MoguchiyDD <ket-ket-01@bk.ru>
2023-12-09 07:34:32 +0600

    Removing

--- NestJS/DaniKo/0/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const port = 3000;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => console.log(`Server started on port ${port}`));
-}
-bootstrap();
