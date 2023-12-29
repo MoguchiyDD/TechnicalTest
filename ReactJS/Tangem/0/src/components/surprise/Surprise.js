55b24f73c0f2080204c08fbacebf26cf019d53b6
MoguchiyDD <ket-ket-01@bk.ru>
2023-12-29 22:53:46 +0600

    Adding TOP Block

---
+++ ReactJS/Tangem/0/src/components/surprise/Surprise.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const SurpriseTop = props => {
+  const {startData, endData, discount, code} = props;
+  return(
+    <div className="surprise-top">
+      <p>
+        <strong>Black Friday</strong>, {startData}-{endData} Nov
+        <span className="gray">&nbsp;ꞏ&nbsp;</span>
+        <strong><span className="yellow">{discount}</span></strong>
+        <span className="gray">&nbsp;ꞏ&nbsp;</span>
+        Use code <strong><span className="yellow">{code}</span></strong> at checkout
+      </p>
+      <figure>
+        <img src={process.env.PUBLIC_URL + "/images/surprise.png"} alt="Surprise Top" />
+      </figure>
+    </div>
+  )
+}
+export const SurpriseBottom = () => {}
