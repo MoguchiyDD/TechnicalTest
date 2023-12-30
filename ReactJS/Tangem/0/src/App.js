24a3775f22cb5c3b3008bd72360f363e13cab78e
MoguchiyDD <ket-ket-01@bk.ru>
2023-12-30 18:25:21 +0600

    Adding SurpriseBottom

--- ReactJS/Tangem/0/src/App.js
+++ ReactJS/Tangem/0/src/App.js
@@ -1,10 +1,15 @@
 import './style/scss/global.scss';
 import './style/scss/surprise.scss';
-import { SurpriseTop } from './components/surprise/Surprise';
+import { SurpriseTop, SurpriseBottom } from './components/surprise/Surprise';
 
 function App() {
+  const surpriseDiscount = "10%OFF";
+  const surpriseCode = "10FRIDAY";
   return (
-    <SurpriseTop startData="24" endData="27" discount="10%OFF" code="10FRIDAY" />
+    <div className="surprise">
+      <SurpriseTop startData="24" endData="27" discount={surpriseDiscount} code={surpriseCode} />
+      <SurpriseBottom discount={surpriseDiscount} code={surpriseCode} />
+    </div>
   );
 }
 
