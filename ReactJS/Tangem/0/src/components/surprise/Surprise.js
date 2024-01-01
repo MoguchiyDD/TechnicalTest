87ab3ec6d2fa29a5bd9404824b2114f0b510a196
MoguchiyDD <ket-ket-01@bk.ru>
2024-01-01 14:58:07 +0600

    Adding COMMENTS && Update SurpriseTop

--- ReactJS/Tangem/0/src/components/surprise/Surprise.js
+++ ReactJS/Tangem/0/src/components/surprise/Surprise.js
@@ -1,9 +1,41 @@
-import React from 'react';
+/*
+Developer & Owner: МогучийДД (MoguchiyDD)
+LICENSE: MIT License which is located in the text file LICENSE
 
+Goal: Determine SCREEN Size
+Result: Accurate SCREEN Size
+
+Past Modification: Adding COMMENTS
+Last Modification: Editing The «SurpriseTOP» CONSTANT
+Modification Date: 2024.01.01, 02:57 PM
+
+Create Data: 2023.12.29, 02:37 PM
+*/
+
+
+import React from "react";
+import { useScreenWidth } from "../screen/Screen";
+
+
+/**
+ * @copyright Copyright (c) 2023 MoguchiyDD
+ * @license MIT License
+ * @description The SURPRISE from Above
+ * @param {*} props DATA
+ * @returns JSX
+ */
 export const SurpriseTop = props => {
-  const {startData, endData, discount, code} = props;
-  return(
-    <div className="surprise-top">
+  const {discount, code} = props;
+
+  let text = "";
+  let btn = "";
+
+  const screenWidth = useScreenWidth();  
+  if (screenWidth > 960) {
+    const startData = 24;
+    const endData = 27;
+
+    text = (
       <p>
         <strong>Black Friday</strong>, {startData}-{endData} Nov
         <span className="gray">&nbsp;ꞏ&nbsp;</span>
@@ -11,17 +43,51 @@ export const SurpriseTop = props => {
         <span className="gray">&nbsp;ꞏ&nbsp;</span>
         Use code <strong><span className="yellow">{code}</span></strong> at checkout
       </p>
-      <button id="btn">Shop now</button>
+    );
+    btn = <button id="btn">Shop now</button>;
+  } else if ((screenWidth >= 768) && (screenWidth <= 960)) {
+    text = (
+      <p>
+        <strong>Black Friday</strong>
+        <span className="gray">&nbsp;ꞏ&nbsp;</span>
+        <strong><span className="yellow">{discount}</span></strong>
+        <span className="gray">&nbsp;ꞏ&nbsp;</span>
+        Use code <strong><span className="yellow">{code}</span></strong>
+      </p>
+    );
+    btn = <button id="btn">Shop now</button>;
+  } else {
+    text = (
+      <p>
+        <strong>Black Friday</strong>,&nbsp;
+        <strong><span className="yellow">{discount}</span></strong>
+      </p>
+    );
+    btn = <button id="btn">❯</button>;
+  }
+
+  return (
+    <div className="surprise-top">
       <figure>
         <img src={process.env.PUBLIC_URL + "/images/surprise.png"} alt="Surprise Top" />
       </figure>
+      {text}
+      {btn}
     </div>
   )
 }
+
+
+/**
+ * @copyright Copyright (c) 2023 MoguchiyDD
+ * @license MIT License
+ * @description The SURPRISE from Below
+ * @param {*} props DATA
+ * @returns JSX
+ */
 export const SurpriseBottom = props => {
   const {discount, code} = props;
-  const urlImage = process.env.PUBLIC_URL + "/images/surprise.png";
-  console.log(urlImage);
+
   return(
     <div className="surprise-bottom">
       <figure>
