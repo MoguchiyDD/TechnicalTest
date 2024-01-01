0cfea1d4971ca6e8fcd813bfea159904958ba0e9
MoguchiyDD <ket-ket-01@bk.ru>
2024-01-01 14:56:23 +0600

    Update APP for SURPRISE

--- ReactJS/Tangem/0/src/App.js
+++ ReactJS/Tangem/0/src/App.js
@@ -5,9 +5,10 @@ import { SurpriseTop, SurpriseBottom } from './components/surprise/Surprise';
 function App() {
   const surpriseDiscount = "10%OFF";
   const surpriseCode = "10FRIDAY";
+
   return (
     <div className="surprise">
-      <SurpriseTop startData="24" endData="27" discount={surpriseDiscount} code={surpriseCode} />
+      <SurpriseTop discount={surpriseDiscount} code={surpriseCode} />
       <SurpriseBottom discount={surpriseDiscount} code={surpriseCode} />
     </div>
   );
