4248ed28c2f1e7f1ec89cc956ce0b17c6d80cf29
MoguchiyDD <ket-ket-01@bk.ru>
2024-01-01 23:17:07 +0600

    Update SCREEN

--- ReactJS/Tangem/0/src/components/screen/Screen.js
+++ ReactJS/Tangem/0/src/components/screen/Screen.js
@@ -5,9 +5,9 @@ LICENSE: MIT License which is located in the text file LICENSE
 Goal: Determine SCREEN Size
 Result: Accurate SCREEN Size
 
-Past Modification: Adding COPYRIGHT
-Last Modification: Adding The «useScreenWidth» FUNCTION
-Modification Date: 2024.01.01, 03:00 PM
+Past Modification: Adding The «useScreenWidth» FUNCTION
+Last Modification: Editing The «useScreenWidth» FUNCTION (LOGIC)
+Modification Date: 2024.01.01, 11:12 PM
 
 Create Data: 2023.12.31, 07:03 PM
 */
@@ -19,20 +19,20 @@ import { useEffect, useState } from "react";
 /**
  * @copyright Copyright (c) 2023 MoguchiyDD
  * @license MIT License
- * @description The SURPRISE from Above
+ * @description The COMPONENT from Above
  * @returns Display SCREEN Width
  */
 export function useScreenWidth() {
   const [width, setWidth] = useState(window.innerWidth);
 
-  function handleResize() {
-    setWidth(window.innerWidth);
-  }
-
   useEffect(() => {
-    window.addEventListener("reaize", handleResize);
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [width]);
 
   return width;
 }
