d89e6645eb59d0da29a1e6eb7a145fe281277994
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-02 01:25:19 +0500

    Adding VARIABLES : 19% && 24%

--- ReactJS/Valantis/0/tailwind.config.js
+++ ReactJS/Valantis/0/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "19pr": "19%",
+        "24pr": "24%"
+      }
+    },
   },
   plugins: [],
 }
