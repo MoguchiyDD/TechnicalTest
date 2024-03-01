7915f055a2228538d8bf018bcd4f6afac75c4ad8
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-01 16:46:15 +0500

    Update ROUTING

--- ReactJS/Valantis/0/src/App.jsx
+++ ReactJS/Valantis/0/src/App.jsx
@@ -4,12 +4,12 @@ import {
   createBrowserRouter,
   createRoutesFromElements
 } from "react-router-dom"
-import Shop from "./components/Shop"
+import { Shop, shopDataLoader } from "./pages/Shop"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
-      <Route index element={<Shop />} />
+      <Route path="shop" index element={<Shop />} loader={shopDataLoader} />
     </Route>
   )
 )
