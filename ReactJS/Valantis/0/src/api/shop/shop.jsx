91e6471dfbf9b5420cbd4c9c054369b099ce411c
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-01 16:48:55 +0500

    Update API Shop

--- ReactJS/Valantis/0/src/api/shop/shop.jsx
+++ ReactJS/Valantis/0/src/api/shop/shop.jsx
@@ -1,12 +1,31 @@
 import md5 from "md5"
 
-const password = "Valantis"
+const password = import.meta.env.VITE_API_SHOP_PASSWORD
 const date = new Date().toISOString().slice(0, 10).split("-").join("")
 const auth = md5(`${password}_${date}`).toString()
 
+const sleep = ms => new Promise(r => setTimeout(r, ms))
+const whileSleep = async (func, data) => {
+  let funcData = null
+
+  while ((funcData === null) || (funcData === undefined)) {
+    funcData = await func(data)
+    sleep(500)
+  }
+
+  return funcData
+}
+
+/**
+ * @copyright Copyright (c) 2023 MoguchiyDD
+ * @license MIT License
+ * @description Using URL «http://api.valantis.store:40000/» Takes The Necessary DATA
+ * @param object body Body for Sending DATA to API
+ * @returns Data Result || undefined
+ */
 const templateFetchShop = async (body) => {
   try {
-    const response = await fetch("http://api.valantis.store:40000/", {
+    const response = await fetch(import.meta.env.VITE_API_SHOP_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -25,14 +44,41 @@ const templateFetchShop = async (body) => {
   }
 }
 
-export const getShopData = async () => {
-  const body = { action: "get_ids" }
+/**
+ * @copyright Copyright (c) 2023 MoguchiyDD
+ * @license MIT License
+ * @description Gets PRODUCT IDs (LIMIT 50 ITEMS)
+ * @returns Object IDs || undefined
+ */
+const getShopIds = async (offset) => {
+  const body = {
+    action: "get_ids",
+    "params": {"offset": offset, "limit": 50}
+  }
   const getIds = await templateFetchShop(body)
 
   if (getIds !== undefined) {
       const ids = [...new Set(getIds)]
-      console.log(getIds.length, ids.length)
+      return ids
   }
 }
 
-export default getShopData
+/**
+ * @copyright Copyright (c) 2023 MoguchiyDD
+ * @license MIT License
+ * @description Gets PRODUCTS by IDs
+ * @returns Object PRODUCTS || undefined
+ */
+const getShopData = async (offset) => {
+  const getIds = await whileSleep(getShopIds, offset)  // Get IDs
+
+  const body = {
+    action: "get_items",
+    "params": {"ids": getIds}
+  }
+  const getData = await whileSleep(templateFetchShop, body)  // Get PRODUCTS
+
+  return getData
+}
+
+export { getShopData }
