6fe0558f414a73adebf9d21704c0a6b77432f282
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-02 15:52:32 +0500

    Removing «<></>»

--- ReactJS/Valantis/0/src/components/shop/Shop.jsx
+++ ReactJS/Valantis/0/src/components/shop/Shop.jsx
@@ -8,18 +8,16 @@ function ShopData({page, data}) {
         {
           data.map((dt, index) => {
             return (
-              <>
-                <div className="w-full h-96 p-5 grid content-center bg-white text-center border-t-8 border-blue-600 rounded transition ease-in-out delay-150 hover:shadow-xl duration-300" key={index}>
-                  <p className="pb-4 text-8xl 2xl:text-9xl">🌌</p>
-                  <p className="pb-2 text-xs lg:text-sm 2xl:text-base font-bold text-gray-400">{dt.id}</p>
-                  <p className="text-xl lg:text-2xl font-bold text-gray-900">{dt.product}</p>
-                  <div className="grid grid-cols-2 relative top-8 lg:top-10">
-                    <p className="text-sm lg:text-base 2xl:text-lg text-gray-600">{dt.brand}</p>
-                    <p className="text-base lg:text-lg 2xl:text-lg text-blue-600">₽{dt.price}</p>
-                  </div>
-                  <br />
+              <div className="w-full h-96 p-5 grid content-center bg-white text-center border-t-8 border-blue-600 rounded transition ease-in-out delay-150 hover:shadow-xl duration-300" key={`${index}`}>
+                <p className="pb-4 text-8xl 2xl:text-9xl">🌌</p>
+                <p className="pb-2 text-xs lg:text-sm 2xl:text-base font-bold text-gray-400">{dt.id}</p>
+                <p className="text-xl lg:text-2xl font-bold text-gray-900">{dt.product}</p>
+                <div className="grid grid-cols-2 relative top-8 lg:top-10">
+                  <p className="text-sm lg:text-base 2xl:text-lg text-gray-600">{dt.brand}</p>
+                  <p className="text-base lg:text-lg 2xl:text-lg text-blue-600">₽{dt.price}</p>
                 </div>
-              </>
+                <br />
+              </div>
             )
           })
         }
@@ -36,42 +34,36 @@ function ShopPagination(page, productsLength) {
 
   if (absPage === 1) {  // /shop?page=1 | previous
     return (
-      <>
-        <div className="my-5 flex justify-between">
-          <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md opacity-50">Прерыдущий</button>
-          <p className="p-3 text-base xl:text-lg 2xl:text-xl text-gray-900">{absPage}</p>
-          <a href={`/shop?page=${nextPage}`}>
-            <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md transition ease-in-out delay-150 hover:bg-purple-700 duration-300">Следующий</button>
-          </a>
-        </div>
-      </>
-    )
-  } else if (productsLength < 50) {  // /shop?page=<number> where PRODUCTS < LIMIT 50 | next
-    return (
-      <>
-        <div className="my-5 flex justify-between">
-          <a href={`/shop?page=${prevPage}`}>
-            <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md transition ease-in-out delay-150 hover:bg-purple-700 duration-300">Предыдущий</button>
-          </a>
-          <p className="p-3 text-base xl:text-lg 2xl:text-xl text-gray-900">{absPage}</p>
-          <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md opacity-50">Следующий</button>
-        </div>
-      </>
-    )
-  }
-
-  return (
-    <>
       <div className="my-5 flex justify-between">
-        <a href={`/shop?page=${prevPage}`}>
-          <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md transition ease-in-out delay-150 hover:bg-purple-700 duration-300">Предыдущий</button>
-        </a>
+        <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md opacity-50">Прерыдущий</button>
         <p className="p-3 text-base xl:text-lg 2xl:text-xl text-gray-900">{absPage}</p>
         <a href={`/shop?page=${nextPage}`}>
           <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md transition ease-in-out delay-150 hover:bg-purple-700 duration-300">Следующий</button>
         </a>
       </div>
-    </>
+    )
+  } else if (productsLength < 50) {  // /shop?page=<number> where PRODUCTS < LIMIT 50 | next
+    return (
+      <div className="my-5 flex justify-between">
+        <a href={`/shop?page=${prevPage}`}>
+          <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md transition ease-in-out delay-150 hover:bg-purple-700 duration-300">Предыдущий</button>
+        </a>
+        <p className="p-3 text-base xl:text-lg 2xl:text-xl text-gray-900">{absPage}</p>
+        <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md opacity-50">Следующий</button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="my-5 flex justify-between">
+      <a href={`/shop?page=${prevPage}`}>
+        <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md transition ease-in-out delay-150 hover:bg-purple-700 duration-300">Предыдущий</button>
+      </a>
+      <p className="p-3 text-base xl:text-lg 2xl:text-xl text-gray-900">{absPage}</p>
+      <a href={`/shop?page=${nextPage}`}>
+        <button className="p-3 text-sm xl:text-base 2xl:text-lg text-white bg-blue-600 rounded-md transition ease-in-out delay-150 hover:bg-purple-700 duration-300">Следующий</button>
+      </a>
+    </div>
   )
 }
 
