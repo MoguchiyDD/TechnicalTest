4868823e9a915cc58094a1883364ab9b877c28aa
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-02 22:24:50 +0500

    Update URL for QUERY to API SHOP

--- ReactJS/Valantis/0/src/pages/Shop.jsx
+++ ReactJS/Valantis/0/src/pages/Shop.jsx
@@ -1,53 +1,98 @@
-import { Suspense } from "react"
+import { Suspense, useEffect } from "react"
 import { getShopData, getShopFilters } from "../api/shop/shop"
 import { Await, useLoaderData, defer } from "react-router"
 import ShopData from "../components/shop/Shop"
 import { SkeletonShopData, SkeletonShopFilterBrand, SkeletonShopFilterPrice } from "../skeletons"
 import { ShopFilterBrand, ShopFilterPrice, ShopFilterProduct } from "../components/shop/filters/ShopFilter"
 
+let url = import.meta.env.VITE_SITE_URL
 let offset = 0
 
 function Shop() {
   const { page, products, brands, prices } = useLoaderData()
+
+  const handleClick = async () => {
+    let curUrl = window.location.href
+
+    const product = document.getElementById("shop-product")
+    const price = document.getElementById("shop-price")
+    const brand = document.querySelectorAll("input[name=\"shop-brand\"]")
+
+    const replaceSpaces = (string) => { return string.split(" ").join("%20") }
+    const filter = (strFilter, query, condition, filter) => {
+      if (condition === true) {
+        const _filter = replaceSpaces(filter)
+        if (query !== null) {
+          curUrl = curUrl.replace(`${strFilter}=${query}`, `${strFilter}=${_filter}`)
+        } else {
+          curUrl += `&${strFilter}=${filter}`
+        }
+      } else {
+        curUrl = curUrl.split(`&${strFilter}=${query}`).join("")
+      }
+    }
+
+    if ((product !== null) && (price !== null) && (brand !== null)) {
+      const _prices = await prices
+
+      const _params = new URLSearchParams(window.location.search)
+      const queryProduct = _params.get("product")
+      const queryPrice = _params.get("price")
+      const queryBrand = _params.get("brand")
+
+      const isProduct = product.value
+      filter("product", queryProduct, isProduct.length >= 1, isProduct)
+
+      const isPrice = price.value
+      filter("price", queryPrice, isPrice > _prices[0], isPrice)
+      
+      const isBrand = document.querySelector("input[name=\"shop-brand\"]:checked").value
+      filter("brand", queryBrand, isBrand !== import.meta.env.VITE_API_SHOP_BRAND_DEFAULT, isBrand)
+
+      url = curUrl
+      window.location.href = url
+    }
+  }
 
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-5 gap-3 2xl:gap-5 p-5 lg:p-3 font-mono">
         <div className="lg:h-screen flex justify-center">
           <div className="w-full lg:w-24pr xl:w-19pr block lg:fixed">
-            <p className="my-5 text-xl lg:text-2xl font-bold text-center text-gray-900">Фильтрация</p>
-              <ShopFilterProduct />
-              <Suspense fallback={<SkeletonShopFilterPrice />}>
-                <Await resolve={prices}>
-                  {
-                    paths => (
-                      <ShopFilterPrice price={paths} />
-                    )
-                  }
-                </Await>
-              </Suspense>
-              <Suspense fallback={<SkeletonShopFilterBrand />}>
-                <Await resolve={brands}>
-                  {
-                    paths => (
-                      <ShopFilterBrand brands={paths} />
-                    )
-                  }
-                </Await>
-              </Suspense>
-            </div>
-          </div>
-          <div className="col-auto lg:col-span-3 xl:col-span-4">
-            <Suspense fallback={<SkeletonShopData />}>
-              <Await resolve={products}>
+            <p className="my-3 lg:my-5 text-xl lg:text-2xl font-bold text-center text-gray-900">Фильтрация</p>
+            <ShopFilterProduct />
+            <Suspense fallback={<SkeletonShopFilterPrice />}>
+              <Await resolve={prices}>
                 {
                   paths => (
-                    <ShopData page={page} data={paths} />
+                    <ShopFilterPrice price={paths} />
                   )
                 }
               </Await>
             </Suspense>
+            <Suspense fallback={<SkeletonShopFilterBrand />}>
+              <Await resolve={brands}>
+                {
+                  paths => (
+                    <ShopFilterBrand brands={paths} />
+                  )
+                }
+              </Await>
+            </Suspense>
+            <button className="mt-4 p-3 lg:p-4 w-full text-sm xl:text-base 2xl:text-lg font-bold text-center text-white bg-blue-600 rounded-md focus:outline-none transition ease-in-out delay-150 hover:bg-purple-700 duration-300" onClick={handleClick}>{"Запрос".toUpperCase()}</button>
           </div>
+        </div>
+        <div className="col-auto lg:col-span-3 xl:col-span-4">
+          <Suspense fallback={<SkeletonShopData />}>
+            <Await resolve={products}>
+              {
+                paths => (
+                  <ShopData url={url} data={paths} />
+                )
+              }
+            </Await>
+          </Suspense>
+        </div>
       </div>
     </>
   )
@@ -56,6 +101,7 @@ function Shop() {
 const shopDataLoader = async () => {
   const params = new URLSearchParams(window.location.search)
   const page = params.get("page")
+  url += "/shop?" + params
 
   if (page !== null) {  // Offset
     const absPage = Math.abs(page)
