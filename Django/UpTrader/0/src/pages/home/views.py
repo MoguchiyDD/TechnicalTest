75a1e0a6ae0e16acd8c73e86b61ae7cea928c2dc
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-06 18:13:51 +0500

    Adding HOME PAGE : UpTrader

---
+++ Django/UpTrader/0/src/pages/home/views.py
@@ -0,0 +1,43 @@
+# Developer && Owner: МогучийДД (MoguchiyDD)
+# LICENSE: MIT License which is located in the text file LICENSE
+#
+# Goal: URL Page for HOME
+# Result: Ready URL Page for HOME
+#
+# Past Modification: Editing The ALL FUNCTIONS (RESULT)
+# Last Modification: Adding DOCUMENTATION
+# Modification Date: 2024.03.06, 05:51 PM
+#
+# Create Date: 2024.03.05, 12:38 PM
+
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+PAGE = "home"
+
+
+# ------------ HOME ------------
+
+def index(request: WSGIRequest) -> HttpResponse:
+    """
+    The «home» TAB from The MENU
+
+    ---
+    PARAMETERS:
+    - request: WSGIRequest -> Request to URL page
+    ---
+    RESULT: Response with DATA for HTML
+    """
+
+    template = "index.html"
+    tab = "Дом"
+
+    context = {
+        "title": tab
+    }
+    return render(request, f"pages/{ PAGE }/{ template }", context)
+
+# ------------------------------
