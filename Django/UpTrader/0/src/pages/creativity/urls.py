18ec875c65c41e21c2028c09d3cff2b5ed95337b
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-06 23:33:09 +0500

    Adding NEW PAGES : UpTrader

--- Django/UpTrader/0/src/pages/creativity/urls.py
+++ Django/UpTrader/0/src/pages/creativity/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="creativity"),
     path("paintings/", views.paintings, name="paintings"),
+    path("paintings/subjects", views.subjects, name="subjects"),
+    path("paintings/objects", views.objects, name="objects"),
     path("sculptures/", views.sculptures, name="sculptures")
 ]
