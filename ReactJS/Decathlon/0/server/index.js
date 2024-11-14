9bbc56181f408eccfae612c7ab152d08d23c9b6d
MoguchiyDD <ket-ket-01@bk.ru>
2024-11-15 02:29:19 +0500

    feat: server - main

---
+++ ReactJS/Decathlon/0/server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const timekeepingRouter = require('./api/timekeeping')(io);
+app.use('/api', timekeepingRouter);
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
